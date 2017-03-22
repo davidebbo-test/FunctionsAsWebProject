0dbd905065a78c2cd1858d57868b30c73c822c46
David Ebbo <david.ebbo@gmail.com>
2017-03-21 17:13:09 -0700

    New CPU heavy function

---
+++ FunctionsAsWebProject/HttpTriggerHighCPU/HttpTriggerHighCPU.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.Threading.Tasks;
+
+namespace FunctionsLibraryProject
+{
+    public class HttpTriggerHighCPU
+    {
+        public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, TraceWriter log)
+        {
+            int max = 100000000;
+
+            for (int i = 0; i < max; i++)
+            {
+                // Just spin CPU to simulate CPU intensive activity
+            }
+
+            return req.CreateResponse(HttpStatusCode.OK, $"Spinned {max} times");
+        }
+    }
+}
